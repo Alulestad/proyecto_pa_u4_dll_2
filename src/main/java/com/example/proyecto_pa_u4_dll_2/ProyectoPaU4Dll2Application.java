5fc19ed1b39bdf8371525f25a31670edf5b7a320
Alulestad <vladimirlestad@gmail.com>
2023-02-28 19:43:30 -0500

    taller36

---
+++ src/main/java/com/example/proyecto_pa_u4_dll_2/ProyectoPaU4Dll2Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_pa_u4_dll_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU4Dll2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU4Dll2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto_pa_u4_dll_2/ProyectoPaU4Dll2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_pa_u4_dll_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaU4Dll2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
